15bebcc306ffe8022137ae29746dbfd2ee779a8e
amanray-commits <57846264+amanray-commits@users.noreply.github.com>
2025-04-07 17:30:25 +0530

    Update Jharkhand_model_2024_GE.sql
    Dynamic Table

--- models/example/Jharkhand_model_2024_GE.sql
+++ models/example/Jharkhand_model_2024_GE.sql
@@ -1,2 +1,10 @@
-{{ config(materialized='view') }}
+{{ config(
+    materialized="dynamic_table",
+    on_configuration_change="apply" | "continue" | "fail",
+    target_lag="downstream" | "<integer> seconds | minutes | hours | days",
+    snowflake_warehouse="<warehouse-name>",
+    refresh_mode="AUTO" | "FULL" | "INCREMENTAL",
+    initialize="ON_CREATE" | "ON_SCHEDULE", 
+
+) }}
 SELECT PARTY,SUM(TOTAL_VOTES) AS TOTAL_VOTES_SECURED FROM GENERAL_ELECTIONS_2024 WHERE STATE = 'Jharkhand' GROUP BY ALL
