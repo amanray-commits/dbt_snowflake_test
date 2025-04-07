2a96fc2f89f0e2ad1df73ccfd60f507801dd82ed
Aman Ray <amankumarray123@gmail.com>
2025-04-07 12:01:54 +0000

    Jharkhand

--- models/example/Jharkhand_model_2024_GE.sql
+++ models/example/Jharkhand_model_2024_GE.sql
@@ -1,2 +1,6 @@
-{{ config(materialized='view') }}
+{{
+    config(
+        materialized='dynamic_table'
+    )
+}}
 SELECT PARTY,SUM(TOTAL_VOTES) AS TOTAL_VOTES_SECURED FROM GENERAL_ELECTIONS_2024 WHERE STATE = 'Jharkhand' GROUP BY ALL
